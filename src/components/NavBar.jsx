541ae1fa716f2cc001db272e9aaee0089b738642
LaupWing <loc_strict@hotmail.com>
2021-11-26 12:42:05 +0100

    contact page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {Link} from 'gatsby'
 
 const NavBar = () => {
    return (
       <nav>
-         
+         <ul>
+            <li>
+               <Link to="/">Home</Link>
+            </li>
+            <li>
+               <Link to="/about">about</Link>
+            </li>
+            <li>
+               <Link to="/contact">contact</Link>
+            </li>
+            <li>
+               <Link to="/tags">tags</Link>
+            </li>
+            <li>
+               <Link to="/recipes">recipes</Link>
+            </li>
+         </ul>
       </nav>
    )
 }
