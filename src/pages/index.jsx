bf73f72792b9e7d78aaaa0327834d1630cf2ce78
LaupWing <loc_strict@hotmail.com>
2021-12-13 19:37:45 +0100

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import {StaticImage} from 'gatsby-plugin-image'
 
 const Home = () => {
    return (
       <Layout>
          <h2>Home page</h2>
+         {/* <StaticImage/> */}
       </Layout>
    )
 }
